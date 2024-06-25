b77866872ed3f8a1b8606389b8687847e1a8e8c3
kim jong chul <jckim@iconloop.com>
2024-06-25 18:00:32 +0900

    add writeObject(), readObject()

--- vesting/src/main/java/io/havah/contract/VestingScheduleType.java
+++ vesting/src/main/java/io/havah/contract/VestingScheduleType.java
@@ -4,19 +4,24 @@ import score.ObjectReader;
 import score.ObjectWriter;
 
 public enum VestingScheduleType {
-    Onetime("onetime"),
-    Linear("linear"),
-    Periodic("periodic"),
-    Daily("daily_condition"),
-    Weekly("weekly_condition"),
-    Monthly("monthly_condition"),
-    Yearly("yearly_condition");
+    Onetime,
+    Linear,
+    Periodic,
+    Daily,
+    Weekly,
+    Monthly,
+    Yearly;
 
-    private final String msg;
-    VestingScheduleType(String _msg) {
-        this.msg = _msg;
+    public static void writeObject(ObjectWriter w, VestingScheduleType vs) {
+        w.beginList(1);
+        w.write(vs.name());
+        w.end();
     }
-    public String getMessage() {
-        return msg;
+
+    public static VestingScheduleType readObject(ObjectReader r) {
+        r.beginList();
+        String name = r.readString();
+        r.end();
+        return VestingScheduleType.valueOf(name);
     }
 }
